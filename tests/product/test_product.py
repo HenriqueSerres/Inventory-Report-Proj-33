d4bf7c19328dd7693605c8ca017249ea9a1d6516
HenriqueSerres <henriqueserres@gmail.com>
2022-09-20 15:41:24 -0300

    req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    fake_product = Product(
+        1,
+        "xablau",
+        "Xablau S/A",
+        "09-20-2021",
+        "10-20-2022",
+        "turma17",
+        "estude muito",
+    )
+
+    assert fake_product.id == 1
+    assert fake_product.nome_do_produto == "xablau"
+    assert fake_product.nome_da_empresa == "Xablau S/A"
+    assert fake_product.data_de_fabricacao == "09-20-2021"
+    assert fake_product.data_de_validade == "10-20-2022"
+    assert fake_product.numero_de_serie == "turma17"
+    assert fake_product.instrucoes_de_armazenamento == "estude muito"
