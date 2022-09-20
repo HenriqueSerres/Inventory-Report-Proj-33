2b5e0346f863ad8a2f578242568bbae13a00dfd3
HenriqueSerres <henriqueserres@gmail.com>
2022-09-20 16:08:06 -0300

    req 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    fake_product = Product(
+        1,
+        "xablau",
+        "Xablau S/A",
+        "09-20-2021",
+        "10-20-2022",
+        "turma17",
+        "estude muito",
+    )
+    assert fake_product.__repr__ == (
+        "O produto xablau"
+        " fabricado em 09-20-2021"
+        " por Xablau S/A com validade"
+        " até 10-20-2022"
+        " precisa ser armazenado estude muito."
+    )
+
+    
+
+
